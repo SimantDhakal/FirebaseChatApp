3471a1ccb1fd6e7c1537525cd3923f14abf7d56b
Simant Dhakal <Simant.dhakal@gmail.com>
2020-05-01 17:02:41 +0545

    Chat app - Initial Commit

---
+++ app/src/main/java/com/simant/firebasechatapp/MainActivity.java
@@ -0,0 +1,223 @@
+package com.simant.firebasechatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView mListView;
+    public static int SIGN_IN_REQUEST_CODE = 10;
+    private DatabaseReference databaseReference;
+    private FirebaseListAdapter<ChatMessage> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = findViewById(R.id.list_of_messages);
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        } else {
+//            Toast.makeText(this,
+//                    "Welcome " + FirebaseAuth.getInstance()
+//                            .getCurrentUser()
+//                            .getDisplayName(),
+//                    Toast.LENGTH_LONG)
+//                    .show();
+            // Load chat room contents
+            displayChatMessages();
+        }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = findViewById(R.id.input);
+                String message = input.getText().toString();
+                if (message.equals("")) {
+                } else {
+                    FirebaseDatabase.getInstance()
+                            .getReference()
+                            .push()
+                            .setValue(new ChatMessage(input.getText().toString(),
+                                    FirebaseAuth.getInstance()
+                                            .getCurrentUser()
+                                            .getDisplayName())
+                            );
+                    input.setText("");
+                }
+            }
+        });
+    }
+
+    private void displayChatMessages() {
+        // load message
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        FirebaseListOptions<ChatMessage> options = new FirebaseListOptions.Builder<ChatMessage>()
+                .setQuery(databaseReference, ChatMessage.class)
+                .setLayout(R.layout.message)
+                .build();
+        adapter = new FirebaseListAdapter<ChatMessage>(options) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                TextView messageText, messageUser, messageTime;
+                messageText = v.findViewById(R.id.message_text);
+                messageUser = v.findViewById(R.id.message_user);
+                messageTime = v.findViewById(R.id.message_time);
+
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+
+                if (FirebaseAuth.getInstance().getCurrentUser().getDisplayName().equals(messageUser.getText())) {
+                    messageUser.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+
+                    // set Gravity of message
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT);
+                    params.weight = 1.0f;
+                    params.gravity = Gravity.LEFT;
+                    messageText.setLayoutParams(params);
+
+                    // setMargin
+                    setMargins(messageText, 0, 0, 90, 0);
+                } else {
+                    messageText.setBackgroundResource(R.drawable.list_message_other_user);
+                    messageUser.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+
+                    // set Gravity of message
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT);
+                    params.weight = 1.0f;
+                    params.gravity = Gravity.RIGHT;
+                    messageText.setLayoutParams(params);
+
+                    // setMargin
+                    setMargins(messageText, 90, 0, 0, 0);
+                }
+
+            }
+        };
+        mListView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+
+                // Close the app
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_sign_out) {
+            AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(MainActivity.this,
+                                    "You have been signed out.",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+
+                            // Close activity
+                            finish();
+                        }
+                    });
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            adapter.startListening();
+        }catch (Exception e){
+
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try{
+            adapter.stopListening();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    // method for margin
+    private void setMargins (View view, int left, int top, int right, int bottom) {
+        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            p.setMargins(left, top, right, bottom);
+            view.requestLayout();
+        }
+    }
+
+    // push notification
+
+}
